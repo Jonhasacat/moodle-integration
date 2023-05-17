66dc90e3134b3c4c684985eb0110851244d9354e
Jon <jonhasacat8@gmail.com>
2023-05-16 21:16:33 -0700

    Academy updates

--- MoodleHelper.py
+++ MoodleHelper.py
@@ -1,4 +1,6 @@
 import os
+from typing import Union
+
 from dotenv import load_dotenv
 from app.helpers.moodle import moodle_api
 
@@ -10,6 +12,18 @@ moodle_api.KEY = os.environ.get('MOODLE_API_KEY')
 
 class MoodleHelper:
     @classmethod
+    def get_all_categories(cls):
+        return moodle_api.call('core_course_get_categories',
+                               criteria=[{'key': 'parent', 'value': 0}],
+                               addsubcategories=0)
+
+    @classmethod
     def get_all_courses(cls):
         return moodle_api.call('core_course_get_courses')
+
+    @classmethod
+    def get_quizzes_by_courses(cls, course_ids: Union[list[int], int]):
+        if isinstance(course_ids, int):
+            course_ids = [course_ids]
+        return moodle_api.call('mod_quiz_get_quizzes_by_courses',  courseids=course_ids)
 
